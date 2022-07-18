66a5c795dfae17334945e070d106aabb07d4acfa
simon.ralek <simon.ralek@adminit.cz>
2022-07-18 20:35:00 +0200

    Routing

--- js/main.js
+++ js/main.js
@@ -1,179 +1,170 @@
-;(function () {
+(function () {
+  var language;
+  function getLanguage() {
+    localStorage.getItem("language") == null ? setLanguage("cs") : false;
+    $.ajax({
+      url: localStorage.getItem("language") + ".json",
+      dataType: "jsonp",
+      dataType: "jsonp",
+      success: function (lang) {
+        language = lang;
+      },
+    });
+  }
 
-/* 	const routes = {
-		'/' : cs,
-		'/en' : en
-	}
+  function setLanguage(lang) {
+    localStorage.setItem("language", lang);
+  }
+  getLanguage();
 
-	const onNavigate = (pathName) => {
-		window.history.pushState(
-			{},
-			pathName,
-			window.location.origin + pathName
-		)
-		rootDiv.innerHtml = routes[pathName]
-	} */
-	
-	var language; 
-	function getLanguage() {
-		(localStorage.getItem('language') == null) ? setLanguage('cs') : false;
-		$.ajax({ 
-			url:  localStorage.getItem('language') + '.json', 
-			dataType: 'jsonp', dataType: 'jsonp', 
-			success: function (lang) { language = lang } });
-	}
+  ("use strict");
 
-	function setLanguage(lang) {
-		localStorage.setItem('language', lang);
-	}
-getLanguage();
-	
-	'use strict';
+  var isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+      );
+    },
+  };
 
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
+  var fullHeight = function () {
+    if (!isMobile.any()) {
+      $(".js-fullheight").css("height", $(window).height());
+      $(window).resize(function () {
+        $(".js-fullheight").css("height", $(window).height());
+      });
+    }
+  };
 
-	
-	var fullHeight = function() {
+  // Parallax
+  var parallax = function () {
+    $(window).stellar();
+  };
 
-		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
-			});
-		}
-	};
+  var contentWayPoint = function () {
+    var i = 0;
+    $(".animate-box").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("animated-fast")
+        ) {
+          i++;
 
-	// Parallax
-	var parallax = function() {
-		$(window).stellar();
-	};
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .animate-box.item-animate").each(function (k) {
+              var el = $(this);
+              setTimeout(
+                function () {
+                  var effect = el.data("animate-effect");
+                  if (effect === "fadeIn") {
+                    el.addClass("fadeIn animated-fast");
+                  } else if (effect === "fadeInLeft") {
+                    el.addClass("fadeInLeft animated-fast");
+                  } else if (effect === "fadeInRight") {
+                    el.addClass("fadeInRight animated-fast");
+                  } else {
+                    el.addClass("fadeInUp animated-fast");
+                  }
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+                  el.removeClass("item-animate");
+                },
+                k * 100,
+                "easeInOutExpo"
+              );
+            });
+          }, 50);
+        }
+      },
+      { offset: "85%" }
+    );
+  };
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
-				i++;
+  var goToTop = function () {
+    $(".js-gotop").on("click", function (event) {
+      event.preventDefault();
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+      $("html, body").animate(
+        {
+          scrollTop: $("html").offset().top,
+        },
+        500,
+        "easeInOutExpo"
+      );
 
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
+      return false;
+    });
 
-							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
-					});
-					
-				}, 50);
-				
-			}
+    $(window).scroll(function () {
+      var $win = $(window);
+      if ($win.scrollTop() > 200) {
+        $(".js-top").addClass("active");
+      } else {
+        $(".js-top").removeClass("active");
+      }
+    });
+  };
 
-		} , { offset: '85%' } );
-	};
+  var pieChart = function () {
+    $(".chart").easyPieChart({
+      scaleColor: false,
+      lineWidth: 4,
+      lineCap: "butt",
+      barColor: "#FF9000",
+      trackColor: "#f5f5f5",
+      size: 160,
+      animate: 1000,
+    });
+  };
 
+  var skillsWayPoint = function () {
+    if ($("#fh5co-skills").length > 0) {
+      $("#fh5co-skills").waypoint(
+        function (direction) {
+          if (direction === "down" && !$(this.element).hasClass("animated")) {
+            setTimeout(pieChart, 400);
+            $(this.element).addClass("animated");
+          }
+        },
+        { offset: "90%" }
+      );
+    }
+  };
 
+  // Loading page
+  var loaderPage = function () {
+    setTimeout(() => {
+      $(".fh5co-loader").fadeOut(900);
+    }, 200);
+  };
 
-	var goToTop = function() {
-
-		$('.js-gotop').on('click', function(event){
-			
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
-
-		$(window).scroll(function(){
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
-			} else {
-				$('.js-top').removeClass('active');
-			}
-
-		});
-	
-	};
-
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-
-	};
-
-
-	// Loading page
-	var loaderPage = function() {
-		setTimeout(() => {
-			$(".fh5co-loader").fadeOut(900);
-		}, 200);
-	};
-
-	
-	$(function(){
-		contentWayPoint();
-		goToTop();
-		loaderPage();
-		fullHeight();
-		parallax();
-		// pieChart();
-		skillsWayPoint();
-	});
-
-
-}());
+  $(function () {
+    contentWayPoint();
+    goToTop();
+    loaderPage();
+    fullHeight();
+    parallax();
+    // pieChart();
+    skillsWayPoint();
+  });
+})();
