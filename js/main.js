ed81c284f5c389f9269c7692d677f4272ffabd4f
test <test>
2022-07-23 21:43:08 +0200

    Changes to toggle theme

--- js/main.js
+++ js/main.js
@@ -130,6 +130,36 @@
       );
     }
   };
+const chk = document.getElementById("chk");
+
+chk.addEventListener("change", () => {
+  var light = document.getElementById("theme-light");
+  var dark = document.getElementById("theme-dark");
+  if (light.disabled) {
+    light.removeAttribute("disabled");
+    dark.setAttribute("disabled", "true");
+    Cookies.set('theme','light');
+    if(isMobile.any) {
+      $(".fh5co-loader").show();
+				setTimeout(() => {
+					$(".fh5co-loader").fadeOut(300);
+				}, 80);
+    }
+				
+  } else {
+    light.setAttribute("disabled", "true");
+    dark.removeAttribute("disabled");
+    Cookies.set('theme','dark');
+    if(isMobile.any){
+      $(".fh5co-loader").show();
+				setTimeout(() => {
+					$(".fh5co-loader").fadeOut(300);
+				}, 80);
+    }
+				
+  }
+});
+
 
   // Loading page
   var loaderPage = function () {
